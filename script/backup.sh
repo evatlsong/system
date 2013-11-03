f38f68f1739515d89ded1b865910c25d7faf9e62
evatlsong <evatlsong@gmail.com>
2013-11-03 22:36:52 +0800

    init

---
+++ script/backup.sh
@@ -0,0 +1,14 @@
+rsync -azvr --delete /home/evatlsong/Music /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /home/evatlsong/Pictures /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /home/evatlsong/Documents /media/evatlsong/WD/evatlsong/
+rsync -azvr /home/evatlsong/Videos /media/evatlsong/WD/evatlsong/
+
+rsync -azvr --delete /home/evatlsong/workspace /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /home/evatlsong/file /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /home/evatlsong/book /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /home/evatlsong/project /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /home/evatlsong/software /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /home/evatlsong/Dropbox /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /home/evatlsong/c_workspace /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /home/evatlsong/system /media/evatlsong/WD/evatlsong/
+rsync -azvr --delete /opt /media/evatlsong/WD/
