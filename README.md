062ae7131ae92b146021686eb46d331a9e139a6a
evatlsong <evatlsong@gmail.com>
2014-04-24 16:48:13 +0800

    change readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 system
 ======
+wget https://raw.githubusercontent.com/evatlsong/system/master/config/.vimrc ~/.vimrc
 
 config system
--- script/setConfig.sh
+++ script/setConfig.sh
@@ -1,17 +1,7 @@
 #! /bin/sh
-if [-e ~/.bashrc]
-then
-    mv ~/.bashrc ~/.bashrc.bak
-fi
 if [-e ~/.vimrc]
 then
     mv ~/.vimrc ~/.vimrc.bak
 fi
-if [-e ~/.gvimrc]
-then
-    mv ~/.gvimrc ~/.gvimrc.bak
-fi
 
-ln -sv ~/system/config/.bashrc ~/.bashrc
 ln -sv ~/system/config/.vimrc ~/.vimrc
-ln -sv ~/system/config/.vimrc ~/.gvimrc
