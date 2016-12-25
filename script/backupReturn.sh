472ed2583e343156e38f908815730e9f89fa061a
evatlsong <evatlsong@gmail.com>
2016-12-26 02:39:59 +0800

    change

---
+++ script/backupReturn.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+rsync -avrn /home/evatlsong/Other /media/evatlsong/WD/evatlsong/ >/home/evatlsong/Temp/loglog
+
+
--- script/backupUsb.sh
+++ script/backupUsb.sh
@@ -8,14 +8,11 @@ rsync -avr --delete /home/evatlsong/File /media/evatlsong/WD/evatlsong/
 rsync -avr --delete /home/evatlsong/Book /media/evatlsong/WD/evatlsong/
 rsync -avr --delete /home/evatlsong/Project /media/evatlsong/WD/evatlsong/
 rsync -avr --delete /home/evatlsong/SourceCode /media/evatlsong/WD/evatlsong/
-rsync -avr --delete /home/evatlsong/Software /media/evatlsong/WD/evatlsong/
+rsync -avr /home/evatlsong/Software /media/evatlsong/WD/evatlsong/
 rsync -avr --delete /home/evatlsong/Dropbox /media/evatlsong/WD/evatlsong/
-rsync -avr --delete /home/evatlsong/Other /media/evatlsong/WD/evatlsong/
+rsync -avr /home/evatlsong/Other /media/evatlsong/WD/evatlsong/
 
 rsync -avr --delete /home/evatlsong/system /media/evatlsong/WD/evatlsong/
-rsync -avr --delete /home/evatlsong/.jenkins /media/evatlsong/WD/evatlsong/
-rsync -avr --delete /home/evatlsong/.m2 /media/evatlsong/WD/evatlsong/
 rsync -avr --delete /home/evatlsong/.ssh /media/evatlsong/WD/evatlsong/
-rsync -avr --delete /home/evatlsong/.npm /media/evatlsong/WD/evatlsong/
 
 rsync -avr --delete /opt /media/evatlsong/WD/
