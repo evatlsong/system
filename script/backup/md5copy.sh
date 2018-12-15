8bc7c64490d47400e32d1ee626775efb96136d94
evatlsong <evatlsong@gmail.com>
2018-12-15 22:27:19 +0800

    change

---
+++ script/backup/md5copy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 递归目录查找文件
+# 若是文件则计算他的md5 根据md5在目标目录检测并建立文件夹 并将文件拷贝过去 
+
+target="./dest"
+md5:cp() {
+    echo "$1 >> $2"
+    #cp "$1" "$2"
+}
+
+md5:cpfolder() {
+    src=$1
+    for file in ${src}/*
+    do
+    if [ -d "$file" ]
+    then
+      echo "$file is directory"
+      md5:cpfolder $file
+    elif [ -f "$file" ]
+    then
+      echo "$file is file"
+        echo `md5sum $file | cut -d ' ' -f1`
+        hash=`md5sum $file | cut -d ' ' -f1`
+        subfolder=${hash:0:2}
+        fileName=${hash:2}
+        destfolder="${target}/${subfolder}"
+        if [ ! -d "${destfolder}" ]; then
+            echo "mkdir foler ${destfolder}"
+            mkdir -p "${destfolder}"
+        fi
+        md5:cp "$file" "${destfolder}/${fileName}.${file##*.}"
+    fi
+    done
+}
+
+md5:cpfolder ./nerc-solr-config
